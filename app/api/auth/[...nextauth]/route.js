5e7610b47b5bb6d27303be2d4f87b0eac5499547
Rishu Kumar <rishukr0123@gmail.com>
2023-05-09 00:57:39 +0530

    web app done

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,10 +1,8 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
-// console.log({
-//      clientId: process.env.GOOGLE_ID,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-// })
+import User from '@models/user';
+import { connectToDB } from '@utils/database';
 
 const handler = NextAuth({
   providers: [
@@ -13,7 +11,8 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     })
   ],
-   async session({ session }) {
+  callbacks: {
+    async session({ session }) {
       // store the user id from MongoDB to session
       const sessionUser = await User.findOne({ email: session.user.email });
       session.user.id = sessionUser._id.toString();
@@ -21,8 +20,8 @@ const handler = NextAuth({
       return session;
     },
     async signIn({ account, profile, user, credentials }) {
-        try {
-             await connectToDB();
+      try {
+        await connectToDB();
 
         // check if user already exists
         const userExists = await User.findOne({ email: profile.email });
@@ -31,17 +30,18 @@ const handler = NextAuth({
         if (!userExists) {
           await User.create({
             email: profile.email,
-            username: profile.name.replace(" ", "").toLowerCase(),
+           username: profile.name.replaceAll(" ", "").toLowerCase(),
             image: profile.picture,
           });
         }
 
         return true
-        } catch (error) {
-             console.log("Error checking if user exists: ", error.message);
-             return false
-        }
-          },
+      } catch (error) {
+        console.log("Error checking if user exists: ", error.message);
+        return false
+      }
+    },
   }
-)
+})
+
 export { handler as GET, handler as POST }
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,55 +4,58 @@ import Link from "next/link";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
-const Nav = () => {
-    const { data: session } = useSession();
-    const [providers,setProviders]=useState(null);
-    const [toggleDropdown, setToggleDropdown] = useState(false);
 
-    useEffect(() => {
-        (async () => {
-        const res = await getProviders();
-        setProviders(res);
-        })();
-    }, []);
+const Nav = () => {
+  const { data: session } = useSession();
+
+  const [providers, setProviders] = useState(null);
+  const [toggleDropdown, setToggleDropdown] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const res = await getProviders();
+      setProviders(res);
+    })();
+  }, []);
 
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
-    <Link href='/' className='flex gap-2 flex-center'>
+      <Link href='/' className='flex gap-2 flex-center'>
         <Image
-        src='/assets/images/logo.svg'
-        alt='logo'
-        width={30}
-        height={30}
-        className='object-contain'
+          src='/assets/images/logo.svg'
+          alt='logo'
+          width={30}
+          height={30}
+          className='object-contain'
         />
         <p className='logo_text'>RKtopia</p>
-    </Link>
-    {/* Desktop Navigation */}
-    <div className='sm:flex hidden'>
-    {session?.user ? (
-        <div className='flex gap-3 md:gap-5'>
-        <Link href='/create-prompt' className='black_btn'>
-            Create Post
-        </Link>
+      </Link>
 
-        <button type='button' onClick={signOut} className='outline_btn'>
-            Sign Out
-        </button>
+      {/* Desktop Navigation */}
+      <div className='sm:flex hidden'>
+        {session?.user ? (
+          <div className='flex gap-3 md:gap-5'>
+            <Link href='/create-prompt' className='black_btn'>
+              Create Post
+            </Link>
 
-        <Link href='/profile'>
-            <Image
-            src="assets/images/logo.svg"
-            width={37}
-            height={37}
-            className='rounded-full'
-            alt='profile'
-            />
-        </Link>
-        </div>
+            <button type='button' onClick={signOut} className='outline_btn'>
+              Sign Out
+            </button>
+
+            <Link href='/profile'>
+              <Image
+                src={session?.user.image}
+                width={37}
+                height={37}
+                className='rounded-full'
+                alt='profile'
+              />
+            </Link>
+          </div>
         ) : (
-        <>
-        {providers &&
+          <>
+            {providers &&
               Object.values(providers).map((provider) => (
                 <button
                   type='button'
@@ -60,26 +63,29 @@ const Nav = () => {
                   onClick={() => {
                     signIn(provider.id);
                   }}
-                  className='black_btn'>
+                  className='black_btn'
+                >
                   Sign in
                 </button>
               ))}
-             </>
-    )}
-    </div>
-     {/* Mobile Navigation */}
+          </>
+        )}
+      </div>
+
+      {/* Mobile Navigation */}
       <div className='sm:hidden flex relative'>
         {session?.user ? (
-            <div className="flex">
-                  <Image
-            src="assets/images/logo.svg"
-            width={37}
-            height={37}
-            className='rounded-full'
-            alt='profile'
-            onClick={() => setToggleDropdown(!toggleDropdown)}
+          <div className='flex'>
+            <Image
+              src={session?.user.image}
+              width={37}
+              height={37}
+              className='rounded-full'
+              alt='profile'
+              onClick={() => setToggleDropdown(!toggleDropdown)}
             />
-             {toggleDropdown && (
+
+            {toggleDropdown && (
               <div className='dropdown'>
                 <Link
                   href='/profile'
@@ -107,10 +113,10 @@ const Nav = () => {
                 </button>
               </div>
             )}
-            </div>
-        ):(
+          </div>
+        ) : (
           <>
-        {providers &&
+            {providers &&
               Object.values(providers).map((provider) => (
                 <button
                   type='button'
@@ -118,15 +124,16 @@ const Nav = () => {
                   onClick={() => {
                     signIn(provider.id);
                   }}
-                  className='black_btn'>
+                  className='black_btn'
+                >
                   Sign in
                 </button>
               ))}
-             </>
+          </>
         )}
       </div>
+    </nav>
+  );
+};
 
-    </nav>)
-}
-
-export default Nav
+export default Nav;
